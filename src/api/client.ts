f98de7d299b2c3a049a36a835a32bf88d0b7a0ca
hthinh24 <hungthinh240305@gmail.com>
2026-01-23 20:18:26 +0700

    add env file

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
         "Content-Type": "application/json",
     },
@@ -69,7 +69,7 @@ axiosClient.interceptors.response.use(
                     throw new Error('No refresh token available');
                 }
 
-                const response = await axios.post('http://localhost:8080/api/v1/auth/refresh', {
+                const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/v1/auth/refresh`, {
                     refreshToken: refreshToken
                 });
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_BACKEND_BASE_URL: string;
+  readonly VITE_BACKEND_URL: string;
 
   readonly VITE_ZEGO_APP_ID: string;
   readonly VITE_ZEGO_SERVER_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
